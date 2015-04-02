88f7eae73d1416f42f90b6ee6cd395fbe8e41278
sheepmansLab <tsu23hiroc@msn.com>
2015-04-03 00:52:37 +0900

    20150402_04

---
+++ FamiCal/src/jp/sheepman/famical/view/CustomNumberPicker.java
@@ -0,0 +1,80 @@
+package jp.sheepman.famical.view;
+
+import jp.sheepman.famical.R;
+import android.app.Activity;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import com.androidquery.AQuery;
+
+public class CustomNumberPicker extends LinearLayout {
+	private AQuery aq;
+	
+	/**
+	 * 初期化処理
+	 * @param context
+	 * @param attrs
+	 */
+	public CustomNumberPicker(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		LayoutInflater inflator = ((Activity)context).getLayoutInflater();
+		View view = inflator.inflate(R.layout.layout_numpick_view, null);
+		this.aq = new AQuery(view);
+		aq.id(R.id.btnNumpickUp).clicked(lsnrButtonUp);
+		aq.id(R.id.btnNumpickDown).clicked(lsnrButtonDown);
+		setValue(0);
+		addView(view);
+	}
+	
+	/**
+	 * Upボタン押下時
+	 */
+	OnClickListener lsnrButtonUp = new OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			addNumber(1);
+		}
+	};
+	
+	/**
+	 * Downボタン押下時イベント
+	 */
+	OnClickListener lsnrButtonDown = new OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			addNumber(-1);
+		}
+	};
+
+	/**
+	 * 値を取得する
+	 * @return
+	 */
+	public int getValue(){
+		return Integer.valueOf(aq.id(R.id.etCount).getText().toString());
+	}
+	
+	/**
+	 * 値をセットする
+	 * @param value
+	 */
+	public void setValue(int value){
+		if(value < 0){
+			value = 0;
+		}
+		aq.id(R.id.etCount).text(String.valueOf(value));
+	}
+	
+	/**
+	 * カウンタに値を加算する
+	 * @param num	加算値
+	 */
+	private void addNumber(int num){
+		int value = Integer.valueOf(aq.id(R.id.etCount).getText().toString());
+		value = value + num;
+		setValue(value);
+	}
+}
