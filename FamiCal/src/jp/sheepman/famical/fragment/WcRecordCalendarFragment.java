58b3ba1f9d35837967cb2e6acc9d058d199ee527
Sheepman <sheepmanshut@gmail.com>
2015-04-28 16:33:49 +0900

    2015042801

--- FamiCal/src/jp/sheepman/famical/fragment/WcRecordCalendarFragment.java
+++ FamiCal/src/jp/sheepman/famical/fragment/WcRecordCalendarFragment.java
@@ -1,7 +1,6 @@
 package jp.sheepman.famical.fragment;
 
 import java.util.Calendar;
-import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 
@@ -11,17 +10,18 @@ import jp.sheepman.common.util.CalendarUtil;
 import jp.sheepman.famical.R;
 import jp.sheepman.famical.form.WcRecordForm;
 import jp.sheepman.famical.model.WcRecordSelectModel;
-import jp.sheepman.famical.util.CommonConst;
 import android.annotation.SuppressLint;
 import android.graphics.Color;
 import android.graphics.Rect;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.View.OnTouchListener;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver.OnGlobalLayoutListener;
 import android.view.animation.AlphaAnimation;
 import android.widget.LinearLayout;
 import android.widget.TableLayout;
@@ -29,17 +29,17 @@ import android.widget.TableRow;
 import android.widget.TextView;
 
 public class WcRecordCalendarFragment extends BaseFragment {
-	private LayoutInflater inflator;
-	private Calendar cal;
+	private LayoutInflater mInflator;
+	private Calendar mCalendarBase;
 	
 	private int family_id;
 	private Calendar wc_record_date;
 	
-	int statusBarHeight;
-	View frm;
+	private View ｍFrmSelectedCell;
 	
 	@Override
 	public void onSaveInstanceState(Bundle outState) {
+		Log.d("famical","onSaveInstanceState");
 		super.onSaveInstanceState(outState);
 		if(outState != null){
 			outState.putInt("family_id", family_id);
@@ -51,43 +51,50 @@ public class WcRecordCalendarFragment extends BaseFragment {
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-		if(savedInstanceState == null){
-			if(getArguments() != null){
-				savedInstanceState.putAll(getArguments());
-			}
-		}
+		Log.d("famical","onCreate");
 		super.onCreate(savedInstanceState);
-		cal = Calendar.getInstance();
-		cal.setTime(new Date());
+		mCalendarBase = CalendarUtil.getToday();
+		wc_record_date = CalendarUtil.getToday();
+		
+		//引数を受け取る
+		if(getArguments() != null){
+			family_id = getArguments().getInt("family_id");
+			wc_record_date = CalendarUtil.str2cal(getArguments().getString("wc_record_date"));
+		}
 	}
-
+	
 	@SuppressLint("InflateParams")
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		this.inflator = inflater;
+		Log.d("famical","onCreateView");
+		this.mInflator = inflater;
 		//Viewを生成
-		View v = inflator.inflate(R.layout.fragment_calebdar, null);
+		View v = mInflator.inflate(R.layout.fragment_calebdar, null);
 		//選択表示用の枠
-		this.frm = v.findViewById(R.id.vSelectedFrame);
+		this.ｍFrmSelectedCell = v.findViewById(R.id.vSelectedFrame);
 		
-		createCalendarView(v, cal, false);
+		createCalendarView(v, mCalendarBase, false);
 		
 		return v;
 	}
+	
 
 	/**
 	 * カレンダーViewをセットする
 	 * @param v
-	 * @param cal
+	 * @param vCal
 	 */
 	@SuppressLint("InflateParams")
-	private void createCalendarView(View v, Calendar cal, boolean doAmimation) {
-		int year = CalendarUtil.getYear(cal);
-		int month = CalendarUtil.getMonth(cal);
+	private void createCalendarView(View v, Calendar vCal, boolean doAmimation) {
+		//処理用にカレンダーを複製する
+		Calendar vCalTemp = CalendarUtil.clone(vCal);
+		
+		int year = CalendarUtil.getYear(vCalTemp);
+		int month = CalendarUtil.getMonth(vCalTemp);
 		
 		//選択表示を消す
-		this.frm.setVisibility(View.GONE);
+		this.ｍFrmSelectedCell.setVisibility(View.GONE);
 		
 		// ヘッダをセット
 		LinearLayout llHeader = (LinearLayout) v.findViewById(R.id.llCalendarHeader);
@@ -109,12 +116,12 @@ public class WcRecordCalendarFragment extends BaseFragment {
 		//データ取得
 		WcRecordSelectModel model = new WcRecordSelectModel(getActivity());
 		WcRecordForm form = new WcRecordForm();
-		form.setWc_record_date(cal);
+		form.setWc_record_date(vCalTemp);
 		List<BaseForm> list = model.selectByMonth(form);
 		
 		// 初日にリセット
-		cal.set(Calendar.DATE, 1);
-		cal.add(Calendar.DATE, (-1) * (cal.get(Calendar.DAY_OF_WEEK) - 1));
+		vCalTemp.set(Calendar.DATE, 1);
+		vCalTemp.add(Calendar.DATE, (-1) * (vCalTemp.get(Calendar.DAY_OF_WEEK) - 1));
 
 		// カレンダーを作成
 		for (int i = 0; i < tl.getChildCount(); i++) {
@@ -127,11 +134,11 @@ public class WcRecordCalendarFragment extends BaseFragment {
 				int countDisable = 0;
 				for (int j = 0; j < 7; j++) {
 					// 日付セルのViewを生成
-					View cell = inflator.inflate(R.layout.layout_calendar_cell, null);
+					final View cell = mInflator.inflate(R.layout.layout_calendar_cell, null);
 					// 行にViewを追加
 					tr.addView(cell, p);
 					// 前月、次月の場合は対象外
-					if (month == CalendarUtil.getMonth(cal)) {
+					if (month == CalendarUtil.getMonth(vCalTemp)) {
 						//当月のみマークを表示
 						((TextView)cell.findViewById(R.id.tvCelPeCircle)).setText(R.string.lblMarkCircle);
 						((TextView)cell.findViewById(R.id.tvCelPoCircle)).setText(R.string.lblMarkCircle);
@@ -141,15 +148,15 @@ public class WcRecordCalendarFragment extends BaseFragment {
 						
 						// セルの日付をセット
 						((TextView) cell.findViewById(R.id.tvCellDay))
-								.setText(String.valueOf(CalendarUtil.getDate(cal)));
+								.setText(String.valueOf(CalendarUtil.getDate(vCalTemp)));
 						// 付加情報として日付をセット
-						cell.setTag(CalendarUtil.cal2str(cal));
+						cell.setTag(CalendarUtil.cal2str(vCalTemp));
 						
 						//その日のデータがないかチェックする
 						Iterator<BaseForm> ite = list.iterator();
 						while(ite.hasNext()){
 							WcRecordForm f = (WcRecordForm)ite.next();
-							if(CalendarUtil.cal2str(f.getWc_record_date()).equals(CalendarUtil.cal2str(cal))){
+							if(CalendarUtil.cal2str(f.getWc_record_date()).equals(CalendarUtil.cal2str(vCalTemp))){
 								((TextView)cell.findViewById(R.id.tvCelPeCount)).setText(String.valueOf(f.getPe_count()));
 								((TextView)cell.findViewById(R.id.tvCelPoCount)).setText(String.valueOf(f.getPo_count()));
 								//色を付ける
@@ -161,7 +168,6 @@ public class WcRecordCalendarFragment extends BaseFragment {
 								ite.remove();
 							}
 						}
-						
 						//土日に色を設定する
 						if(j == 6){
 							cell.setBackgroundResource(R.drawable.calendar_cell_sat);
@@ -174,6 +180,11 @@ public class WcRecordCalendarFragment extends BaseFragment {
 						}
 						// 押下時のイベントをセット
 						cell.setOnTouchListener(lsnrTouch);
+						
+						//指定日だった場合色をつける
+						if(vCalTemp.compareTo(wc_record_date) == 0){
+							setSelectedColor(cell);
+						}
 					} else {
 						cell.setBackgroundColor(Color.GRAY);
 						countDisable ++;
@@ -183,12 +194,10 @@ public class WcRecordCalendarFragment extends BaseFragment {
 						tr.setVisibility(View.GONE);
 					}
 					//次の日にする
-					cal.add(Calendar.DATE, 1);
+					vCalTemp.add(Calendar.DATE, 1);
 				}
 			}
 		}
-		//処理前の月の初日に戻す
-		cal.set(year, (month - 1), 1);
 
 		if(doAmimation){
 			AlphaAnimation alpha = new AlphaAnimation(0.5f, 1.0f);
@@ -200,24 +209,41 @@ public class WcRecordCalendarFragment extends BaseFragment {
 	/**
 	 * 選択中のセルの色を変える
 	 */
-	private void setSelectedColor(View cell){
-		frm.setVisibility(View.GONE);
-		//画面情報を取得
-		Rect rect = new Rect();
-		//ステータスバーの高さ
-		getActivity().getWindow().getDecorView().getWindowVisibleDisplayFrame(rect);
-		this.statusBarHeight = rect.top;
-		//セルの座標取得
-		int[] location = new int[2];
-		cell.getLocationInWindow(location);
-		//フレームの座標をセット
-		frm.setX(location[0]);
-		frm.setY(location[1] - this.statusBarHeight);
-		//フレームの大きさをセルにあわせる
-		frm.getLayoutParams().width = cell.getWidth();
-		frm.getLayoutParams().height = cell.getHeight();
-		//セルを表示する
-		frm.setVisibility(View.VISIBLE);
+	private void setSelectedColor(final View cell){
+		Log.d("famical", "setSelectedColor");
+		//画面のレイアウトが決まった段階でレイアウトの計算をする
+		cell.getViewTreeObserver().addOnGlobalLayoutListener(new OnGlobalLayoutListener() {
+			
+			@Override
+			public void onGlobalLayout() {
+				Log.d("famical", "onGlobalLayout");
+				//不要になるのでリスナは削除する
+				cell.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+				
+				//セルの非表示化
+				ｍFrmSelectedCell.setVisibility(View.GONE);
+				
+				//画面情報を取得
+				Rect rect = new Rect();
+				//ステータスバーの高さ
+				getActivity().getWindow().getDecorView().getWindowVisibleDisplayFrame(rect);
+				
+				//セルの座標取得
+				int[] location = new int[2];
+				cell.getLocationInWindow(location);
+				
+				//フレームの座標をセット
+				ｍFrmSelectedCell.setX(location[0]);
+				ｍFrmSelectedCell.setY(location[1] - rect.top);
+				//フレームの大きさをセルにあわせる
+				ｍFrmSelectedCell.getLayoutParams().width = cell.getWidth();
+				ｍFrmSelectedCell.getLayoutParams().height = cell.getHeight();
+				
+				//セルを表示する
+				ｍFrmSelectedCell.setVisibility(View.VISIBLE);
+			}
+		});
+		
 	}
 	
 	/**
@@ -226,8 +252,8 @@ public class WcRecordCalendarFragment extends BaseFragment {
 	private OnClickListener lsnrPrev = new OnClickListener() {
 		@Override
 		public void onClick(View v) {
-			cal.add(Calendar.MONTH, -1);
-			createCalendarView(getView(), cal, true);
+			mCalendarBase.add(Calendar.MONTH, -1);
+			createCalendarView(getView(), mCalendarBase, true);
 		}
 	};
 	
@@ -237,8 +263,8 @@ public class WcRecordCalendarFragment extends BaseFragment {
 	private OnClickListener lsnrNext = new OnClickListener() {
 		@Override
 		public void onClick(View v) {
-			cal.add(Calendar.MONTH, 1);
-			createCalendarView(getView(), cal, true);
+			mCalendarBase.add(Calendar.MONTH, 1);
+			createCalendarView(getView(), mCalendarBase, true);
 		}
 	};
 	
@@ -269,13 +295,13 @@ public class WcRecordCalendarFragment extends BaseFragment {
 					if(Math.abs(x2 - x1) > adjust){
 						//右移動の場合
 						if(x2 > x1){
-							cal.add(Calendar.MONTH, -1);
+							mCalendarBase.add(Calendar.MONTH, -1);
 						//左移動の場合
 						} else {
-							cal.add(Calendar.MONTH, 1);
+							mCalendarBase.add(Calendar.MONTH, 1);
 						}
 						//カレンダー再構築
-						createCalendarView(getView(), cal, true);
+						createCalendarView(getView(), mCalendarBase, true);
 					}
 				}
 				break;
@@ -295,20 +321,27 @@ public class WcRecordCalendarFragment extends BaseFragment {
 	
 	@Override
 	public void callback() {
-		createCalendarView(getView(), cal, false);
+		createCalendarView(getView(), mCalendarBase, false);
 	}
 	
 	/**
-	 * 
+	 * 外部からのカレンダー更新指示
 	 * @param family_id
 	 * @param wc_record_date
 	 */
 	public void changeDate(int family_id, Calendar wc_record_date){
-		//TODO family_idの連携方法を検討
+		//アニメーションの有無は基本"なし"とする
 		boolean doAnimation = false;
-		if(CalendarUtil.getMonth(wc_record_date) != CalendarUtil.getMonth(cal)){
+		
+		this.family_id = family_id;
+		//指定が異なる月であった場合アニメーションする
+		if(CalendarUtil.getMonth(wc_record_date) != CalendarUtil.getMonth(this.wc_record_date)){
 			doAnimation = true;
 		}
+		//日付をFragmentに保持させる
+		this.wc_record_date = wc_record_date;
+		this.mCalendarBase = CalendarUtil.getMonthFirstDate(wc_record_date);
+		
 		createCalendarView(getView(), wc_record_date, doAnimation);
 	}
 }
